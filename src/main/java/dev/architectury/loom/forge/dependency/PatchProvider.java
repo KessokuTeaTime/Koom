b1c22c2424ab4df1b8c3e1f005b8f73f5074b47b
TexTrue <65154269+TexBlock@users.noreply.github.com>
2025-11-30 21:04:52 +0800

    try fix NeoForge 21.10.57-beta and above version patches issues

--- src/main/java/dev/architectury/loom/forge/dependency/PatchProvider.java
+++ src/main/java/dev/architectury/loom/forge/dependency/PatchProvider.java
@@ -24,22 +24,31 @@
 
 package dev.architectury.loom.forge.dependency;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.Reader;
 import java.io.UncheckedIOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.mojang.serialization.JsonOps;
+import dev.architectury.loom.forge.config.InstallProfile;
 import org.gradle.api.Project;
 import org.jetbrains.annotations.Nullable;
 
 import net.fabricmc.loom.configuration.DependencyInfo;
 import net.fabricmc.loom.util.Constants;
 import net.fabricmc.loom.util.FileSystemUtil;
+import net.fabricmc.loom.util.ZipUtils;
 
 public class PatchProvider extends DependencyProvider {
 	private final Path projectCacheFolder;
-	private Path installerJar;
+	private File installerJar;
+	private JsonObject json;
+	private InstallProfile installProfile;
 	private @Nullable Path clientPatches;
 	private @Nullable Path serverPatches;
 
@@ -52,13 +61,27 @@ public class PatchProvider extends DependencyProvider {
 	public void provide(DependencyInfo dependency) throws Exception {
 		init();
 
-		installerJar = dependency.resolveFile().orElseThrow(() -> new RuntimeException("Could not resolve Forge installer")).toPath();
+		installerJar = new File(getExtension().getForgeProvider().getGlobalCache(), "forge-installer.jar");
+		Path installProfileJson = getExtension().getForgeProvider().getGlobalCache().toPath().resolve("forge-installProfile.json");
+
+		if (Files.notExists(installProfileJson) || refreshDeps()) {
+			File resolved = dependency.resolveFile().orElseThrow(() -> new RuntimeException("Could not resolve Forge installer"));
+			Files.copy(resolved.toPath(), installerJar.toPath(), StandardCopyOption.REPLACE_EXISTING);
+			Files.write(installProfileJson, ZipUtils.unpack(resolved.toPath(), "install_profile.json"));
+		}
+
+		try (Reader reader = Files.newBufferedReader(installProfileJson)) {
+			json = new Gson().fromJson(reader, JsonObject.class);
+			installProfile = InstallProfile.CODEC.parse(JsonOps.INSTANCE, json)
+					.getOrThrow(false, msg -> getProject().getLogger().error("Couldn't read installer install profile, {}", msg));
+		}
 	}
 
 	public Path extractClientPatches() {
 		if (clientPatches == null) {
 			clientPatches = projectCacheFolder.resolve("patches-client.lzma");
-			extractPatches(clientPatches, "client.lzma");
+			String binpatchName = installProfile.data().get("BINPATCH").client().replaceFirst("^/data/", "");
+			extractPatches(clientPatches, binpatchName);
 		}
 
 		return clientPatches;
@@ -67,7 +90,8 @@ public class PatchProvider extends DependencyProvider {
 	public Path extractServerPatches() {
 		if (serverPatches == null) {
 			serverPatches = projectCacheFolder.resolve("patches-server.lzma");
-			extractPatches(serverPatches, "server.lzma");
+			String binpatchName = installProfile.data().get("BINPATCH").server().replaceFirst("^/data/", "");
+			extractPatches(serverPatches, binpatchName);
 		}
 
 		return serverPatches;
@@ -97,5 +121,13 @@ public class PatchProvider extends DependencyProvider {
 	@Override
 	public String getTargetConfig() {
 		return Constants.Configurations.FORGE_INSTALLER;
+	}
+
+	public JsonObject getJson() {
+		return json;
+	}
+
+	public InstallProfile getInstallProfile() {
+		return installProfile;
 	}
 }
